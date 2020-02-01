64144b9e1aef11e593cd1332c164692690aeade0
Alan Orther <alan.orther@verve.com>
2020-01-31 19:45:37 -0800

    Change the rds.tf to skip final snapshot when destroying and identify the RDS instance by the name rescale

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,9 +2,14 @@ resource "aws_db_instance" "postgres_rds" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
+  backup_retention_period   = 5
   engine_version       = "9.6"
   instance_class       = "db.t2.micro"
   name                 = "rescale"
+  identifier           = "rescale"
   username             = "postgres"
   password             = var.db_password
+  apply_immediately         = true
+  final_snapshot_identifier = "rescale-postgres-backup"
+  skip_final_snapshot  = true
 }
