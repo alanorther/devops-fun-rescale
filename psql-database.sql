e0e99fac97313ae57097bebe30cd6fa7aab26342
Alan Orther <alan.orther@verve.com>
2020-02-01 07:55:46 -0800

    Update the psql-database.sql to only create tables and insert data if not exist or no conflicts. Update startup.sh to use the new psql-database.sql file. Added .dockerignore file.

--- psql-database.sql
+++ psql-database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE hardware (
+CREATE TABLE IF NOT EXISTS hardware (
    id SERIAL PRIMARY KEY,
    provider VARCHAR (255) NOT NULL,
    name VARCHAR (255) NOT NULL
 );
 
-INSERT INTO hardware (provider, name) VALUES ('Amazon', 'c5');
-INSERT INTO hardware (provider, name) VALUES ('Azure', 'H16mr');
+INSERT INTO hardware (provider, name) VALUES ('Amazon', 'c5') ON CONFLICT (name) DO NOTHING;
+INSERT INTO hardware (provider, name) VALUES ('Azure', 'H16mr') ON CONFLICT (name) DO NOTHING;
 
 
